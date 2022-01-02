eb486a88cf226056db95242e714cb154647a2a54
spaceboundbear <mf.michaelfischer@gmail.com>
2022-01-02 16:03:27 -0600

    test client

---
+++ testClient/client/src/pages/Drive.js
@@ -0,0 +1,19 @@
+import RobotButtons from '../components/RobotButtons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Container } from 'react-bootstrap';
+
+function Drive() {
+  return (
+    <>
+      <Container className="w-auto">
+        <Card.Body className="justify-content-md-center ">
+          <div>
+            <RobotButtons />
+          </div>
+        </Card.Body>
+      </Container>
+    </>
+  );
+}
+
+export default Drive;
