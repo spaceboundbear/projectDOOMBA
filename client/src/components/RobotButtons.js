1ee0ca24b75c625c35bcbc78acaaa090930ef529
spaceboundbear <mf.michaelfischer@gmail.com>
2022-01-02 10:30:48 -0600

    test

--- client/src/components/RobotButtons.js
+++ client/src/components/RobotButtons.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { React, useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import { Card, Button, Row, Col, Image } from 'react-bootstrap';
 
