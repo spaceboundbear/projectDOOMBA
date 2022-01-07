9482a22a76c5156a2fb8a76a321153e45ee09c0d
spaceboundbear <mf.michaelfischer@gmail.com>
2022-01-07 04:58:34 -0600

    small adjust

--- client/src/pages/Drive.js
+++ client/src/pages/Drive.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { io } from 'socket.io-client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Button, Row, Col } from 'react-bootstrap';
