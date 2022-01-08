a6b0e3c94eae03a1f6e914957b90d37d437e3317
spaceboundbear <mf.michaelfischer@gmail.com>
2022-01-08 13:17:09 -0600

    updating secret

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
-
-const secret = 'mysupersecret';
+const secret = process.env.JWT_SECRET;
+// const secret = 'mysupersecret';
 const expiration = '2h';
 
 module.exports = {
