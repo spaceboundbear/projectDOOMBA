b49e3235684b489b92f88dc34f72db621b739471
spaceboundbear <mf.michaelfischer@gmail.com>
2022-01-19 18:50:11 -0600

    final production

--- client/src/pages/Drive.js
+++ client/src/pages/Drive.js
@@ -35,7 +35,7 @@ function Drive() {
             <Card className="col-sm-10 col-md-7 bg-secondary buttons container text-center">
               <img
                 className="my-3 w-100"
-                src="https://b314-2603-8081-6405-a88d-f481-bea5-4372-e850.ngrok.io/?action=stream"
+                src="https://d924-2603-8081-6405-a88d-18bb-cbf7-61d2-b8c3.ngrok.io/?action=stream"
                 alt="stream"
               ></img>
             </Card>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -21,12 +21,11 @@ function Home() {
             updates to oneDrive triggered The Robot Uprising, and there seems to
             be little hope left among the small factions of humans survivors.
             After four long years, there are rumors of one man who is determined
-            to fight back. This unknown man, (It's me), and with the help of
-            Peter, Will, and Alvin, decided to fight fire with fire. Thus,
-            project.DOOMBA was born. All they needed was one man crazy enough to
-            pilot the monster they had created. Jeremy, you are the only man fit
-            to pilot, and the only one who can save us from the EDGE.bots. Good
-            luck, and God Speed.
+            to fight back. This unknown man, and with the help of Peter, Will,
+            and Alvin, decided to fight fire with fire. Thus, project.DOOMBA was
+            born. All they needed was one man crazy enough to pilot the monster
+            they had created. You are the only person fit to pilot, and the only
+            one who can save us from the EDGE.bots. Good luck, and God Speed.
           </p>
         </Card.Body>
       </Card>
